d30c88ee2601dba7ee5d888bb876de0906dedd18
flametaichou <lein.flamesword@gmail.com>
2018-11-11 22:11:58 +0300

    test

--- src/main/java/ru/flametaichou/nerfspawners/MobSpawnHandler.java
+++ src/main/java/ru/flametaichou/nerfspawners/MobSpawnHandler.java
@@ -1,64 +1,75 @@
 package ru.flametaichou.nerfspawners;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.event.world.BlockEvent;
 
 public class MobSpawnHandler {
 	
 	Random random = new Random();
 	
-	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	@SubscribeEvent(priority = EventPriority.LOW)
 	public void onEntitySpawn(EntityJoinWorldEvent event) {
+		if (event.world.isRemote) {
+			return;
+		}
+
 		if (event.world.provider.dimensionId == ConfigHelper.worldID) {
 
-			int radius = ConfigHelper.spawnRadius;
-			int entity_x = new Integer((int) event.entity.posX);
-			int entity_y = new Integer((int) event.entity.posY);
-			int entity_z = new Integer((int) event.entity.posZ);
-			
-			boolean flag = false;
-			for (int x = entity_x - radius; x <= entity_x + radius; x++) {
-				for (int y = entity_y - radius; y <= entity_y + radius; y++) {
-					for (int z = entity_z - radius; z <= entity_z + radius; z++) {
-						Block block = event.world.getBlock(x, y, z);
-						String blockId = new Integer(Block.getIdFromBlock(block)).toString();
-						if (ConfigHelper.spawnersIdList.contains(blockId)) {
-							flag = true;
-							break;
+			if (true) {
+				int radius = ConfigHelper.spawnRadius;
+				int entity_x = new Integer((int) event.entity.posX);
+				int entity_y = new Integer((int) event.entity.posY);
+				int entity_z = new Integer((int) event.entity.posZ);
+
+				boolean flag = false;
+				for (int x = entity_x - radius; x <= entity_x + radius; x++) {
+					for (int y = entity_y - radius; y <= entity_y + radius; y++) {
+						for (int z = entity_z - radius; z <= entity_z + radius; z++) {
+							Block block = event.entity.worldObj.getBlock(x, y, z);
+							String blockId = new Integer(Block.getIdFromBlock(block)).toString();
+							if (ConfigHelper.spawnersIdList.contains(blockId)) {
+								flag = true;
+								break;
+							}
 						}
 					}
 				}
-			}
 
-			if (event.entity instanceof EntityCreature) {
-	        	if (flag) {
-					if (!mobIsBlacklisted(event.entity.getClass().getName().toLowerCase())) {
-						if (ConfigHelper.debugMode) {
-							System.out.println("Cancel spawn blacklist entity: " + event.entity.toString());
+				if (event.entity instanceof EntityCreature) {
+					if (flag) {
+						boolean despawned = false;
+						if (mobIsBlacklisted(event.entity.getClass().getName().toLowerCase())) {
+							if (ConfigHelper.debugMode) {
+								System.out.println("Cancel spawn blacklist entity: " +
+										event.entity.getClass().getName() + " (" +
+										event.entity.toString() + ")"
+								);
+							}
+							event.setCanceled(true);
+							despawned = true;
 						}
-						event.world.onEntityRemoved(event.entity);
-						event.setCanceled(true);
-						return;
-					}
 
-//					if (ConfigHelper.debugMode) {
-//						System.out.println("Spawning: " + event.entity.toString());
-//					}
-
-	        		if (random.nextDouble() > ConfigHelper.chanse) {
-						if (ConfigHelper.debugMode) {
-							System.out.println("Cancel spawn: " + event.entity.toString());
+						if (!despawned && random.nextDouble() > ConfigHelper.chanse) {
+							if (ConfigHelper.debugMode) {
+								System.out.println("Cancel spawn: " +
+										event.entity.getClass().getName() + " (" +
+										event.entity.toString() + ")"
+								);
+							}
+							event.setCanceled(true);
 						}
-						event.world.onEntityRemoved(event.entity);
-						event.setCanceled(true);
 					}
-	        	}
+				}
 			}
 		}
 	}
